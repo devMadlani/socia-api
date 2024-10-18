2a4f3e2df313f4ce67210928c375f238afb7b53f
Dev Madlani <madlanidev@gmail.com>
2024-10-18 09:37:44 +0530

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+//Register
+router.post("/register", async (req, res) => {
+  try {
+    //Generate New Password
+    const salt = await bcrypt.genSalt(10);
+    const hashPass = await bcrypt.hash(req.body.password, salt);
+    //Create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashPass,
+    });
+
+    //save user and return response
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//login
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    console.log(validPass);
+    !validPass && res.status(400).json("wrong password");
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
